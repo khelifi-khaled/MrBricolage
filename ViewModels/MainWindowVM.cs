75c56588c30934ea0a197a2a35d70236d64a0693
Khaled KHELIFI <khaled.khelifi@iramps.email>
2022-12-20 16:21:01 +0100

    Ajoutez des fichiers projet.

---
+++ ViewModels/MainWindowVM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrBricolage.ViewModels
+{
+    public class MainWindowVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// binding  
+        /// </summary>
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }//end OnPropertyChanged
+ 
+
+
+    }//end class 
+}//end namespace 
